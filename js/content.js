233eef7e1a69a7f2c068c34020aed5e5a8c28339
Ben Szucs <benszucs@gmail.com>
2018-07-14 09:03:11 +0100

    add some finishing touches, change the wording

--- js/content.js
+++ js/content.js
@@ -1,35 +1,35 @@
 $(document).ready(function() {
 
   $('#project1').click(function() {
-    $('#modal-projects').load('../projects/portfolio.html').hide().show();
+    $('#modal-projects').load('../content/portfolio.html').hide().show();
   });
 
   $('#project2').click(function() {
-    $('#modal-projects').load('../projects/dsign.html').hide().show();
+    $('#modal-projects').load('../content/dsign.html').hide().show();
   });
 
   $('#project3').click(function() {
-    $('#modal-projects').load('../projects/conquer.html').hide().show();
+    $('#modal-projects').load('../content/conquer.html').hide().show();
   });
 
   $('#project4').click(function() {
-    $('#modal-projects').load('../projects/weather.html').hide().show();
+    $('#modal-projects').load('../content/weather.html').hide().show();
   });
 
   $('#project5').click(function() {
-    $('#modal-projects').load('../projects/movie.html').hide().show();
+    $('#modal-projects').load('../content/movie.html').hide().show();
   });
 
   $('#project6').click(function() {
-    $('#modal-projects').load('../projects/quiz.html').hide().show();
+    $('#modal-projects').load('../content/quiz.html').hide().show();
   });
 
   $('#project7').click(function() {
-    $('#modal-projects').load('../projects/library.html').hide().show();
+    $('#modal-projects').load('../content/library.html').hide().show();
   });
 
   $('#project8').click(function() {
-    $('#modal-projects').load('../projects/calculator.html').hide().show();
+    $('#modal-projects').load('../content/calculator.html').hide().show();
   });
 
 }); //end ready
